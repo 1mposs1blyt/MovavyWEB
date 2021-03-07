68de15afe94e015e465eb3d5185991a4e2f39113
alexandr <alexandr.bryaginya@gmai.com>
2021-03-07 20:14:35 +0700

    12312312312312

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function sayHello(name) {
     alert('Hello ' + name + '!');
     }
+
+
+let links = document.querySelectorAll("nav a");
+
+let path = window.location.pathname;
+ids = path.lastIndexOf('/');
+iddot = path.lastIndexOf('.')
+path = path.substring(ids, iddot);
+console.log(path);
+
+for (let link of links){
+    let attr = link.getAttribute("href")
+    if (path == attr) {
+        link.classList.add('active')
+    }
+}
--- server.js
+++ server.js
@@ -1,65 +1,50 @@
 const http = require('http');
 const fs = require('fs');
-
-const hostname = '192.168.2.20'; // IP
+const hostname = '192.168.30.123'; // IP
 const port = 3000; // порт
-
 const server = http.createServer((req, res) => {
     let url = req.url;
-    if (url == '/') {
+    if(url == '/') {
         url = '/index'
     }
-
     // console.log(url);
-    if (url != '/favicon.ico') {
-
-        if (req.url.endsWith('.css')) {
+    if(url != '/favicon.ico') {
+        if(req.url.endsWith('.css')) {
             let cssFile = req.url.slice(1);
-
             fs.readFile(cssFile, (err, data) => {
-                if (err) throw err;
-
+                if(err) throw err;
                 res.setHeader('Content-Type', 'text/css')
                 res.statusCode = 200;
                 res.write(data);
                 res.end()
             })
-        }
-        else if (req.url.endsWith('.js')) {
+        } else if(req.url.endsWith('.js')) {
             let jsFile = req.url.slice(1);
-
             fs.readFile(jsFile, (err, data) => {
-                if (err) throw err;
-
+                if(err) throw err;
                 res.setHeader('Content-Type', 'text/javascript')
                 res.statusCode = 200;
                 res.write(data);
                 res.end()
             })
-        }
-
-
-        fs.readFile(url.substr(1, url.length) + '.html', function (err, data) {
-            if (!err) {
-                res.writeHead(200, { 'Content-Type': 'text/html', });
-                data = data.toString().replace("{{name}}", "Alexandr")
-                res.write(data)
-                res.end();
-            } else {
-                fs.readFile('404.html', function (err, data) {
-                    res.writeHead(400, { 'Content-Type': 'text/html', });
-                    // res.write(data)
-                    res.end(data);
+        } else {
+            fs.readFile(url.substr(1, url.length) + '.html', function(err, data) {
+                if(!err) {
+                    res.writeHead(200, { 'Content-Type': 'text/html', });
+                    data = data.toString().replace("{{name}}", "Alexandr")
+                    res.write(data)
+                    res.end();
+                } else {
+                    fs.readFile('404.html', function(err, data) {
+                        res.writeHead(400, { 'Content-Type': 'text/html', });
+                        // res.write(data)
+                        res.end(data);
+                    });
                 }
-                );
-            }
-        })
-
+            })
+        }
     }
-}
-);
-
-
+});
 server.listen(port, hostname, () => {
     console.log('Server is running!')
 })
